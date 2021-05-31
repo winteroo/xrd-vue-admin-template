d7656fc6ff5e3ce4848d6622cadd157b5c13ee21
yangw <yangwei957@163.com>
2021-05-31 15:02:09 +0800

    添加代码1

---
+++ .stylelintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  plugins: [],
+  extends: [
+    "stylelint-config-standard",
+    "stylelint-config-standard-scss",
+    'stylelint-config-rational-order'
+  ],
+  rules: {}
+};
